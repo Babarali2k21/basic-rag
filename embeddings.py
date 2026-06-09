d3c21c0473f92cd0775c5ba58bfb8992d36a82a0
Babar Ali <148423037+Babarali2k21@users.noreply.github.com>
2026-06-09 18:57:10 +0200

    add chroma_db to gitignore

--- embeddings.py
+++
@@ -1,8 +0,0 @@
-from openai import OpenAI
-
-def get_openai_embedding(text, client=None):
-    if client is None:
-        client = OpenAI()
-    response = client.embeddings.create(input=text, model="text-embedding-3-small")
-    embedding = response.data[0].embedding
-    return embedding
